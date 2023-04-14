710d62c5cf41aa95ee49c524172138b27eaa9756
Eaglewatch55 <orlandogarcia94@hotmail.com>
2023-04-14 11:39:48 -0600

    Stage 4/8

--- Hangman (Python)/task/hangman/hangman.py
+++ Hangman (Python)/task/hangman/hangman.py
@@ -1,10 +1,11 @@
 import random
 
 print("H A N G M A N")
-input_word = input("Guess the word: ")
 
 word_list = ["python", "java", "swift", "javascript"]
 winner_word = random.choice(word_list)
+filler = '-' * (len(winner_word)- 3)
+input_word = input(f"Guess the word {winner_word[:3] + filler}: ")
 
 if input_word == winner_word:
     print("You survived!")
